f572eb67c1e6e990cd8f5f5e15efb8cd636c47d7
GitKalo <mynameis.kaloyan@gmail.com>
2025-09-02 16:26:07 +0200

    Add outfile content check

--- src/paths_to_counts.py
+++ src/paths_to_counts.py
@@ -11,17 +11,21 @@ import json, glob, os
 if not os.path.exists('out/sim_output_counts') :
     os.makedirs('out/sim_output_counts')
 
-for json_file in glob.glob('out/sim_output_reduced/*.json') :
-    with open(json_file, 'r') as f :
-        data = json.load(f)
+reduced_output_files = glob.glob('out/sim_output_reduced/*.json')
+if reduced_output_files :
+    for json_file in reduced_output_files :
+        with open(json_file, 'r') as f :
+            data = json.load(f)
 
-        data_new = {}
-        for target, paths in data.items() :
-            data_new[target] = {}
-            for path in paths :
-                path_string = '@'.join([str(n) for n in path])
-                data_new[target][path_string] = data_new[target].get(path_string, 0) + 1
+            data_new = {}
+            for target, paths in data.items() :
+                data_new[target] = {}
+                for path in paths :
+                    path_string = '@'.join([str(n) for n in path])
+                    data_new[target][path_string] = data_new[target].get(path_string, 0) + 1
 
-        # for target, paths in data_new.items():
-        with open(f"out/sim_output_counts/{json_file.split('/')[-1]}", 'w') as f :
-            json.dump(data_new, f)
+            # for target, paths in data_new.items():
+            with open(f"out/sim_output_counts/{json_file.split('/')[-1]}", 'w') as f :
+                json.dump(data_new, f)
+else :
+    print("No output files found in 'out/sim_output_reduced'.")
--- src/reduce_paths.py
+++ src/reduce_paths.py
@@ -4,18 +4,22 @@ Reduce the number of paths in each output file by taking the first N paths.
 
 import json, glob, os
 
-N = 100
+N = 100     # Number of paths to keep
 
 if not os.path.exists('out/sim_output_reduced') :
     os.makedirs('out/sim_output_reduced')
 
-for json_file in glob.glob('out/sim_output/*.json') :
-    with open(json_file, 'r') as f :
-        data = json.load(f)
+full_output_files = glob.glob('out/sim_output/*.json')
+if full_output_files :
+    for json_file in full_output_files :
+        with open(json_file, 'r') as f :
+            data = json.load(f)
 
-        data_new = {}
-        for target, paths in data.items() :
-            data_new[target] = paths[:N]
+            data_new = {}
+            for target, paths in data.items() :
+                data_new[target] = paths[:N]
 
-        with open(f"out/sim_output_reduced/{json_file.split('/')[-1]}", 'w') as f :
-            json.dump(data_new, f)
+            with open(f"out/sim_output_reduced/{json_file.split('/')[-1]}", 'w') as f :
+                json.dump(data_new, f)
+else :
+    print("No output files found in 'out/sim_output'.")
