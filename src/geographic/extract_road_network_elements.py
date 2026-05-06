4078e3a94eb91b3e365eba45cf31d1aa8cc2608b
franciscomalveiro <francisco.malveiro@isi.it>
2026-05-06 10:45:44 +0200

    Extract different road network types

--- src/geographic/extract_road_network_elements.py
+++ src/geographic/extract_road_network_elements.py
@@ -5,23 +5,40 @@ Get nodes and edges that form the road network graph out of osm data.
 
 import pyrosm
 
+import argparse
 import tempfile
 import zipfile
+import sys
 import os
 
 
+def argparser():
+    parser = argparse.ArgumentParser(
+                    prog=sys.argv[0],
+                    description='What the program does',
+                    epilog='Text at the bottom of help'
+    )
 
-def main():
-    date = '250905'
+    parser.add_argument('--network_type', type=str, choices=('walking', 'cycling', 'driving', 'driving+service', 'all'), default='all')
+    return parser
 
-    savedir = '/data/big/fmalveiro/complexity72/'
-    zipped_osm = os.path.join(savedir, 'osm_data.zip')
-    zipped_elements = os.path.join(savedir, 'DEFAULT-road_network_elements.zip')
 
-    osmfile = f"somalia-{date}-highways.osm.pbf"
+def main(args):    
+    date = '251124'
 
     node_vars = ['id', 'geometry']
     edge_vars = ['u', 'v', 'highway', 'length', 'geometry']
+
+    network_type = args.network_type
+    
+    
+    savedir = '/data/big/fmalveiro/complexity72/'
+    zipped_osm = os.path.join(savedir, 'osm_data.zip')
+    zipped_elements = os.path.join(savedir, f"DEFAULT-road_network_elements-{network_type}.zip")
+
+    osmfile = f"somalia-{date}-highways.osm.pbf"
+
+
 
     # zip library will write within the zipfile, not overwriting it...
     if os.path.exists(zipped_elements):
@@ -37,12 +54,12 @@ def main():
         parser = pyrosm.OSM(loadpath)      
 
         print('Obtaining road network elements...')
-        nodes_gdf, edges_gdf = parser.get_network(network_type='all', nodes=True)
+        nodes_gdf, edges_gdf = parser.get_network(network_type=network_type, nodes=True)
 
         nodes_gdf = nodes_gdf[node_vars]
         edges_gdf = edges_gdf[edge_vars]
 
-        print(f"There are {nodes_gdf.shape[0]} nodes and {edges_gdf.shape[0]} edges.")
+        print(f"There are {nodes_gdf.shape[0]} nodes and {edges_gdf.shape[0]} edges for network_type={network_type}.")
 
 
         for element, gdf in zip(('nodes', 'edges'), (nodes_gdf, edges_gdf)):
@@ -64,5 +81,7 @@ def main():
 
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__':    
+    parser = argparser()
+    args = parser.parse_args()
+    main(args)
